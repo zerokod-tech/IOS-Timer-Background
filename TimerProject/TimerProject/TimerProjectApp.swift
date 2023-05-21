bcc4d3e991f6c3017282a7ba1a5f9bab044b5862
zerokod-tech <133849255+zerokod-tech@users.noreply.github.com>
2023-05-22 00:55:52 +0200

    commit 1 train IOS app complete
    training IOS NATIVE app complete made with SWIFT, SWIFTUI, XCODE with timer, count down, background color that alternate every 2 seconds

---
+++ TimerProject/TimerProject/TimerProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerProjectApp.swift
+//  TimerProject
+//
+//  Created by Roberta Marino on 20/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TimerProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
